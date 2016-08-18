976477662a08732a33d708518f6c7d8a3be31b90
Yeni <yenicapote08@gmail.com>
2016-08-18 12:22:12 -0700

    initial commit

---
+++ user_input.rb
@@ -0,0 +1,33 @@
+# require "word_game"
+
+puts "Welcome to Word Guess Game!"
+puts "The rules are as follows:
+      Enter one letter at a time
+      If correct, the letter will be filled in
+      If incorrect, you will lose a petal
+      If all your petals are gone, you lose
+      If you guess all correct letters in Word you win!!!"
+
+
+puts "
+(@)(@)(@)(@)(@)
+  ,\,\,|,/,/,
+     _\|/_
+    |_____|
+     |   |
+     |___|
+"
+
+puts "Enter one letter: "
+
+user_guess = gets.chomp.upcase
+
+until /[a-zA-Z]+/.match(user_guess)   #checking for only letter values
+  puts "Invalid. Please enter a letter: "
+  user_guess = gets.chomp.upcase
+end
+first_char = user_guess.chars{|c| c[0]}                  #checking user enters only one character
+puts first_char[0]
+  # until                           #checking user does not enter same character more than once
+  #
+  #   end
---
+++ word_game.rb
@@ -0,0 +1,12 @@
+class WordGame
+attr_accessor
+
+def initialize(words)
+@cat = words[:cat]
+@dog = words[:dog]
+
+end
+
+end
+
+words = {cat: ["C", "A", "T"], dog: ["D", "O", "G"]}
