a1377a94fd9772cca06ce2970043126f63cf4a84
Kelly Tran <trankphuong@gmail.com>
2016-08-18 12:36:13 -0700

    WIP

--- word.rb
+++ word.rb
@@ -7,30 +7,18 @@ class Word
         @screen_output = ['_', '_', '_', '_', '_']
     end
 
+    def replace_dash(guess) # guess is a letter
+        @screen_output.gsub!('_', guess)
+        print "word: " + @screen_output
+    end
 
     def correct(input) # input should be a letter
         answer = Answer.new
-        # if answer.include?(input) true
-        #     index of that letter in answer array
-        #     and replace the _ with that letter
-        #
-        # puts
-
-        # while number_of_guess < 5
-        #     puts "What is your letter guess?"
-        #     guess = gets.chomp
-
         answer.each do |letter|
             if guess == letter
                 puts "You're right!"
-                
-            else
-                puts "You're wrong!"
-                # alter the ACSII art
+                guess.replace_dash(letter)
             end
         end
-
-        # number_of_guess -= 1
-        end
-
     end
+end
