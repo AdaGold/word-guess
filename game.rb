02226b239f72f1b93d41ff794c78b35445fd215b
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-08-18 14:08:48 -0700

    Add win/lose and keep track of letters guessed

--- game.rb
+++ game.rb
@@ -3,8 +3,9 @@ class Game
   def initialize(word)
     @word = word.upcase
     @word_array = @word.scan /\w/
-    @points = 7
+    @points = 3
     @current_status = []
+    @guessed_letters = []
 
     (@word.length).times do
       @current_status << "_"
@@ -13,6 +14,7 @@ class Game
 
   def guess
     @guess = gets.chomp.upcase
+    @guessed_letters << @guess
     check_letter_guess(@guess)
   end
 
@@ -23,17 +25,30 @@ class Game
         puts letter + i.to_s
         @current_status[i] = letter
         match = true
+        if @current_status == @word_array
+          puts "You guessed it!"
+          # exit? break?
+        end
       end
     end
     if match == false
       @points -= 1
+      if lose?
+        puts "Game over. You didn't guess it :(. The secret word was #{ @word }"
+        # exit? break?
+      end
     end
   end
 
   def print_art
     print @current_status
     puts "You have #{@points} points left."
+    print "Guessed letters: #{ @guessed_letters }"
 
+  end
+
+  def lose?
+    @points == 0
   end
 
 end
