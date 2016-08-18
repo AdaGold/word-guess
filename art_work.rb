15b729c34652f4e101901181a6cc6c8dde3ec20c
Kate Shaffer <kate.shaffer@gmail.com>
2016-08-18 12:16:26 -0700

    Initial commit of art_work.rb

---
+++ art_work.rb
@@ -0,0 +1,14 @@
+class ArtWork
+
+
+  def initialize(countdown)
+    @countdown = countdown
+  end
+
+  def balloon_five
+    cat balloon_source.txt
+  end
+end
+
+# a = ArtWork.new(1)
+# a.balloon_five
