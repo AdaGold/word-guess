917855caad89188935c4930133ef69e1858fbffa
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-08-18 16:22:04 -0700

    Updated ASCII art volcanoes and comments.

--- volcano.rb
+++ volcano.rb
@@ -1,22 +1,72 @@
 class Volcano
 
+# add to game directions: try to guess the correct letters of the secret word before the volcano erupts! 
 
-def initialize(score)
-  # use case or if statement to return appropriate image
+  def initialize(score)
+    # use case or if statement to return appropriate image
+
+    # art = volcano.new(points)
+    # case score
+    # when 5
+    #   print ...
+    # when 4
+    #   print ...
+    # ...
+    # end
+
+
+
+  end
+
+  puts "\n                /vvv\\"
+  puts "               /     \\"
+  puts "              /       \\"
+  puts "             /         \\"
+  puts "            /           \\"
+  puts "          /               \\"
+  puts "         /AAAAAAAAAAAAAAAAA\\"
+  puts "        /AAAAAAAAAAAAAAAAAAA\\"
+
+  puts "\n                /vvv\\"
+  puts "               /     \\"
+  puts "              /       \\"
+  puts "             /         \\"
+  puts "            /AAAAAAAAAAA\\"
+  puts "          /AAAAAAAAAAAAAAA\\"
+  puts "         /AAAAAAAAAAAAAAAAA\\"
+  puts "        /AAAAAAAAAAAAAAAAAAA\\"
+
+  puts "\n                /vvv\\"
+  puts "               /     \\"
+  puts "              /AAAAAAA\\"
+  puts "             /AAAAAAAAA\\"
+  puts "            /AAAAAAAAAAA\\"
+  puts "          /AAAAAAAAAAAAAAA\\"
+  puts "         /AAAAAAAAAAAAAAAAA\\"
+  puts "        /AAAAAAAAAAAAAAAAAAA\\"
+
+
+  puts "\n                /AAA\\"
+  puts "               /AAAAA\\"
+  puts "              /AAAAAAA\\"
+  puts "             /AAAAAAAAA\\"
+  puts "            /AAAAAAAAAAA\\"
+  puts "          /AAAAAAAAAAAAAAA\\"
+  puts "         /AAAAAAAAAAAAAAAAA\\"
+  puts "        /AAAAAAAAAAAAAAAAAAA\\"
+
+  puts "\n                      ooO"
+  puts "                     ooOOOo"
+  puts "                   oOOOOOOoooo"
+  puts "                 ooOOOooo  oooo"
+  puts "                /AAA\\"
+  puts "               /AAAAA\\"
+  puts "              /AAAAAAA\\"
+  puts "             /AAAAAAAAA\\          AAAAH! RUN FOR YOUR LIVES!"
+  puts "            /AAAAAAAAAAA\\               /"
+  puts "          /AAAAAAAAAAAAAAA\\   	 o           o"
+  puts "         /AAAAAAAAAAAAAAAAA\\    /-    o     /-"
+  puts "        /AAAAAAAAAAAAAAAAAAA\\  /\\   -/-    /\\"
+  puts "                                    /\\"
+
 end
-
-
-
-                      ooO
-                     ooOOOo
-                   oOOOOOOoooo
-                 ooOOOooo  oooo
-                /vvv\
-               /V V V\
-              /V  V  V\
-             /         \          AAAAH!  RUN FOR YOUR LIVES!
-            /           \               /
-          /               \   	  o          o
-__       /                 \     /-   o     /-
-/\      /AAAAAAAAAAAAAAAAAAA\  /\  -/-    /\
-                                    /\
