171d43a842e665b5defa18004c8354061a07a589
Johna N. Morris <johna.n.morris@gmail.com>
2016-08-18 14:44:48 -0700

    WIP simplify methods.

--- game.rb
+++ game.rb
@@ -1,25 +1,43 @@
 class Game
-attr_accessor :answer, :blanks
+    attr_accessor :answer, :blanks
 
-  def initialize(answer)
-    @answer = answer
-    @blanks = []
-  end
-
-  def get_blanks
-    @answer.length.times do
-      @blanks << "_"
+    def initialize(answer)
+        @answer = answer
+        @blanks = []
+        @flowers = ["(@)", "(@)", "(@)", "(@)", "(@)"]
+        get_blanks
     end
-  end
 
-  def fill_in(guess)
-    answer_letters = @answer.split('') #this gives us an array of the answer letters
-    game_blanks = @blanks
-    answer_letters.each_with_index do |letter, i|
-      if letter == guess
-        game_blanks[i] = guess
-      end
+    def get_blanks
+        @answer.length.times do
+            @blanks << "_"
+        end
     end
-    print game_blanks.join("")
-  end
+
+    def fill_in(guess)
+        answer_letters = @answer.split('') #this gives us an array of the answer letters
+        answer_letters.each_with_index do |letter, i|
+            if letter == guess
+                @blanks[i] = guess
+                counter +=1
+            end
+        end
+#        if counter == 0
+#            remove_flower
+#        end
+        print @blanks.join("")
+    end
+
+    def remove_flower
+        @flowers.pop
+        print @flowers
+#        if @flowers.length == 0
+#            puts "You ran out of flowers. Game over."
+#            exit
+        end
+    end
+
+    def full?
+        @blanks == @answer
+    end
 end
--- word_guess.rb
+++ word_guess.rb
@@ -1,11 +1,12 @@
+require_relative 'game'
+
 words = ["popcorn"]
 
-puts "Please enter a letter."
+word = Game.new(words[0])
+
+print "Please enter a letter. "
 user_guess = gets.chomp.downcase
 
-counter = 5
-while counter > 0
-  if #something matches a letter, fill in a letter
-  else #remove/add are & decrement counter
-  end
+(word.answer.length + 5).times do
+    word.fill_in(user_guess)
 end
