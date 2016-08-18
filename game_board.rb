103e99cc6cc3b9f91fe299b1b1a9eae37c5b5db9
Alma Maizels <aymaiz@yahoo.com>
2016-08-18 15:36:38 -0700

    Minor changes to dash line method, start there

--- game_board.rb
+++ game_board.rb
@@ -2,7 +2,9 @@ require 'terminal-table'
 
 class GameBoard
   def initialize
-
+    @letter_dash = []
+    @the_word = ""
+    @dash_array = Array.new(@the_word.length, "_ ")
   end
 
   def success(good_letters_locs) # needs the array of elements including letters & index
@@ -13,14 +15,20 @@ class GameBoard
     puts "Amazing answer. You are guessing: #{current_letter}"
   end
 
-def dash_line
-  dash_line = ""
-
-# dash.each do |letter|
-#   dash_line = dash_line + "#{ dash[i]} "
-
-end
-end
+# @TODO
+  # def dash_line(good_letters_locs) #array of arrays
+  #   good_letters_locs = @good_letters_with_good_index
+  #   @dash_array.each do |dash|
+  #     @good_letters_with_good_index.each_with_index do |letter, i|
+  #     # pull out each array_element [0]: the letter and [1]: the original index
+  #     # with i we w
+  #
+  #
+  #     # dash.each do |letter|
+  #     #   dash_line = dash_line + "#{ dash[i]} "
+  #
+  #   #end #each
+  # end
 
 end #GameBoard
 
