f6832fde6e89fcf6ffa9d6955efbadf657752c4e
Kate Shaffer <kate.shaffer@gmail.com>
2016-08-18 13:56:35 -0700

    Real balloons! Initial image.

--- art_work.rb
+++ art_work.rb
@@ -1,14 +1,15 @@
 class ArtWork
 
-
   def initialize(countdown)
     @countdown = countdown
   end
 
   def balloon_five
-    cat balloon_source.txt
+    # puts "This is an image of five balloons. Really."
+    five_b = File.open("balloon_source.txt")
+    puts five_b.read
   end
 end
 
-# a = ArtWork.new(1)
-# a.balloon_five
+a = ArtWork.new(1)
+a.balloon_five
