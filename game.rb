b542fbc2ac10a663fbb8fc85301a000f7b3b5b01
guineveresaenger <guineveresaenger@yahoo.com>
2016-08-18 15:07:55 -0700

    Now with flowers

--- game.rb
+++ game.rb
@@ -2,14 +2,12 @@ class Game
   attr_accessor :word, :guessed_word, :remaining_guesses
 
   def initialize
-    @word = "panda"
+    @word = ["panda", "guinevere", "snowcone", "whimsical", "hardcode", "dragon",
+    "sunglasses"].sample
     @guessed_word = ["_"] * @word.length
     @remaining_guesses = 5
     print @guessed_word
     puts "Welcome to word-guess!"
-
-
-
   end
 
   def is_in_word (letter)
@@ -17,10 +15,13 @@ class Game
     word_array.each_with_index do |char, i|
       if letter == char
         @guessed_word[i] = char
-      else
-        @remaining_guesses -= 1
       end
     end
+    if @word.include?(letter) == false
+      @remaining_guesses -= 1
+
+    end
+
     return @guessed_word
   end
 
@@ -40,9 +41,29 @@ class Game
     end
   end
 
+  def flowers
+    case @remaining_guesses
 
+    when 5
+      puts "(@)(@)(@)(@)(@)"
+    when 4
+      puts "(@)(@)(@)(@)"
+    when 3
+      puts "   (@)(@)(@)"
+    when 2
+      puts "   (@)   (@)"
+    when 1
+      puts "   (@)"
+    when 0
+      puts
+    else
+      puts "How did this get messed up!?"
+    end
 
-
-
-
+    puts '  ,\,\,|,/,/,'
+    puts '     _\|/_'
+    puts '    |_____|'
+    puts '     |   |'
+    puts "     |___|"
+  end
 end
--- script.rb
+++ script.rb
@@ -2,11 +2,19 @@
 require_relative 'game.rb'
 game_one = Game.new
 
-puts "Guess your first letter"
-input = gets.chomp.downcase
-
-
+puts game_one.flowers
 while game_one.remaining_guesses > 0
-  puts game_one.is_in_word(input)
-  puts "You still have #{game_one.remaining_guesses} wrong guesses left..."
+  puts "Guess a letter"
+  input = gets.chomp.downcase
+  game_one.is_in_word(input)
+  puts game_one.display_progress
+  puts game_one.flowers
+  if game_one.win?
+    puts "Congrats, you won the game!"
+    exit
+  end
+  puts "You have #{game_one.remaining_guesses} wrong guesses left..."
 end
+
+puts "Sorry, you lost! The word was #{game_one.word}. Better luck next time."
+exit
