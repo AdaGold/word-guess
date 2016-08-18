c0cdefb605899914ec6bfc5d4e017dc62a3bb8e9
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-08-18 15:05:08 -0700

    Started volcano ascii images

---
+++ volcano.rb
@@ -0,0 +1,22 @@
+class Volcano
+
+
+def initialize(score)
+  # use case or if statement to return appropriate image
+end
+
+
+
+                      ooO
+                     ooOOOo
+                   oOOOOOOoooo
+                 ooOOOooo  oooo
+                /vvv\
+               /V V V\
+              /V  V  V\
+             /         \          AAAAH!  RUN FOR YOUR LIVES!
+            /           \               /
+          /               \   	  o          o
+__       /                 \     /-   o     /-
+/\      /AAAAAAAAAAAAAAAAAAA\  /\  -/-    /\
+                                    /\
