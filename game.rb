340779c67eb49e4166ca52ca434cbd0068a3c201
Allison Hoke <allisonhoke@gmail.com>
2016-08-18 12:15:25 -0700

    Added game and word_guess

---
+++ game.rb
@@ -0,0 +1,26 @@
+class Game
+attr_accessor :answer, :blanks
+
+  def initialize(answer)
+    @answer = answer
+    @blanks = []
+  end
+
+  def get_blanks
+    @answer.length.times do
+      @blanks << "_"
+    end
+  end
+
+  def fill_in(guess)
+    answer_letters = @answer.split('') #this gives us an array of the answer letters
+    game_blanks = @blanks
+    answer_letters.each_with_index do |letter, i|
+      if letter[i] == guess
+        game_blanks[i] = guess
+      end
+      puts game_blanks
+    end
+
+  end
+end
---
+++ word_guess.rb
@@ -0,0 +1,11 @@
+words = ["popcorn"]
+
+puts "Please enter a letter."
+user_guess = gets.chomp.downcase
+
+counter = 5
+while counter > 0
+  if #something matches a letter, fill in a letter
+  else #remove/add are & decrement counter
+  end
+end
