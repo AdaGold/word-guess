8856e5d3060e9ffe281afe5e5363a6a85512001d
Yuri Nakashima <yuri310@gmail.com>
2016-08-18 12:16:26 -0700

    initial commit guess game

---
+++ guessing_game.rb
@@ -0,0 +1,32 @@
+def GuessingGame
+
+  def initialize(name)
+    @name = name
+    @answer = ["z", "e", "b", "r", "a"]
+    @num_guess = 10
+    @wr
+    @art = (asci art)
+    puts @art
+  end
+
+  def compare?
+    @answer.include?guess
+  end
+
+  def method to ask user
+    while @num_guess != 0
+      puts "Guess a letter!"
+      @guesses = []
+      guess = gets.chomp.to_s
+      @guesses << guesses
+
+      if compare? == true
+
+
+
+
+
+
+    end
+  end
+end
