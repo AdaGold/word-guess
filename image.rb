b87852e5974d035c20a4d523367509e530dbcfd9
Karin Kubischta <karin.kubischta@gmail.com>
2016-08-18 18:20:41 -0700

    Added some comments/thoughts, added print last line of whale.

--- image.rb
+++ image.rb
@@ -1,5 +1,4 @@
 class Image
-attr_reader :image_array
 
   def initialize
     @image_array = ["▄██████████████▄▐█▄▄▄▄█▌",
@@ -10,7 +9,7 @@ attr_reader :image_array
   end
 
   def print_image(lines) #Take in the number of lines from the user's incorrect answers and print that number of lines of the image array
-      puts @image_array[0..lines-1]
+      puts @image_array[0..lines-1] #If the user gets a correct guess on the first try, this prints the whole whale. Oops. 
 
   end
 
--- program.rb
+++ program.rb
@@ -3,7 +3,7 @@ require_relative 'answer'
 require_relative 'image'
 
 fail_whale = Image.new
-aw = Answer.new("MORDOR")
+aw = Answer.new("MORDOR") #We should probably not put the initalization word here. Options: hardcode it in the initialize method in Answer, or do the optimization of harcoding a list in Answer, and having initialize pick one of the options from that list.
 
 puts "What letter would you like to guess?"
 user_guess = gets.chomp.upcase
@@ -12,12 +12,15 @@ aw.check_user_guess(user_guess)
 
 wrong_answers = aw.num_incorrect
 
-while wrong_answers < fail_whale.image_array.length
+while wrong_answers < fail_whale.image_array.length #TODO: do while loop will do the loop at least once (don't have to initialize variables outside of loop)
   fail_whale.print_image(wrong_answers)
 
   puts "What letter would you like to guess?"
   user_guess = gets.chomp.upcase #We might not need a guess object at all unless we decide to something more than just store users guesses in an array
   aw.check_user_guess(user_guess)
   wrong_answers = aw.num_incorrect
-  #Need to figure out how to print the last line of the whale before the loop ends
+  #checking this will print the last line of the whale before the loop ends
+  if wrong_answers == fail_whale.image_array.length
+    fail_whale.print_image(wrong_answers)
+  end
 end
