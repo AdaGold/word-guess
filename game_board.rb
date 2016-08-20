04e31fcc8710ae89db5915f2ec9d2e1ac007f6fe
Alma Maizels <aymaiz@yahoo.com>
2016-08-20 16:18:21 -0700

    Set up each in dash_line. Added testing puts messages. Program runs without errors.

--- game_board.rb
+++ game_board.rb
@@ -1,4 +1,5 @@
-require 'terminal-table'
+#require 'terminal-table'
+require_relative 'word_guess'
 
 class GameBoard
   def initialize
@@ -11,28 +12,39 @@ class GameBoard
     puts
   end
 
-  def print_game_board(current_letter)
-    puts "Amazing answer. You are guessing: #{current_letter}"
+  def print_game_board(letters) # the good or bad letters arrays
+    puts "Amazing answer. You are guessing: #{letters[0][0].to_s}" #for testing
+    dash_lines(letters) # returns dash_line results
   end
 
 # @TODO
+  def dash_lines(letters)
+    if letters[0].is_a?(Array) #good letters, letters.last[0] is the current input
+      puts "The letter is correct"
+      letters.each do |letter, index_loc|
+        # replace the string "_ " with letter at index_loc
+      end #do
+    elsif letters[0].is_a?(String) #bad letters, letters.last is the current input
+      #countdown -=1 ?
+      puts "The letter is incorrect"
+    else
+      puts "There is an error in the letter counting logic"
+    end
+
+  end #dash_lines
+  
   # def dash_line(good_letters_locs) #array of arrays
   #   good_letters_locs = @good_letters_with_good_index
   #   @dash_array.each do |dash|
   #     @good_letters_with_good_index.each_with_index do |letter, i|
   #     # pull out each array_element [0]: the letter and [1]: the original index
   #     # with i we w
-  #
-  #
-  #     # dash.each do |letter|
-  #     #   dash_line = dash_line + "#{ dash[i]} "
-  #
   #   #end #each
   # end
 
 end #GameBoard
 
-# my_word_guess = WordGuess.new
-# word_guess = my_word_guess.word_logic
-# my_game_board = GameBoard.new
-# my_game_board.print_game_board(word_guess)
+my_word_guess = WordGuess.new
+word_guess = my_word_guess.word_logic
+my_game_board = GameBoard.new
+my_game_board.print_game_board(word_guess)
--- word_guess.rb
+++ word_guess.rb
@@ -1,4 +1,4 @@
-require_relative 'game_board'
+#require_relative 'game_board'
 
 class WordGuess
   #  attr_accessor
@@ -8,7 +8,8 @@ class WordGuess
     @the_word = the_words.sample(1)[0]
     @word_by_letter = @the_word.split('')
     @bad_letters = []
-    #  @letters = []
+    puts "The word is #{ @the_word }" # for testing
+        #  @letters = []
   end
 
   def get_user_input
@@ -22,18 +23,19 @@ class WordGuess
   def word_logic
     letter_guess = get_user_input
     if @word_by_letter.include?(letter_guess)
-      good_letters_locs = []
+      good_letters_at_index_loc = []
       @word_by_letter.each_with_index do |letter, i|
         if letter == letter_guess
-          good_letters_locs << [letter, i]
+          good_letters_at_index_loc << [letter, i]
         end
       end
-      puts "Good letters: " + good_letters_locs.to_s # #for GameBoard/debugging both
+      puts "Good letters: " + good_letters_at_index_loc.to_s # #for GameBoard/debugging both
       # send game_board.success(letter_guess, )
-      return good_letters_locs
+      return good_letters_at_index_loc
     else
       @bad_letters << letter_guess
       puts "Bad letters: " + @bad_letters.to_s # for GameBoard debugging/both
+      return @bad_letters # first forgot this return: caused errors in game_board
       # send game_board.fail(letter_guess)
       # will change bad_letters
     end
