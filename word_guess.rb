d7362b3708c7c243e9b6c5dc3f18650cfec02ffd
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-08-22 12:58:03 -0700

    to be continued--older version

--- word_guess.rb
+++ word_guess.rb
@@ -1,86 +1,100 @@
 class WordGuess
-  attr_accessor :words, :target_word_guess, :correct_guesses, :incorrect_guesses, :target_array_form, :dash_line, :petal_art
+    attr_accessor :words, :target_word_guess, :correct_guesses, :incorrect_guesses, :target_array_form, :dash_line, :petal_art, :your_guess
 
-  def initialize
-    @words =  ["JOY", "SAD", "HAPPY", "FACE"]
-    @target_word_guess = ""
-    @target_array_form = nil
-    @correct_guesses = []
-    @incorrect_guesses = []
-    @dash_line = nil
-    @petal_art = ""
-  end
+    def initialize
+        @words =  ["JOY", "SAD", "HAPPY", "FACE"]
+        @target_word_guess = ""
+        @target_array_form = nil
+        @correct_guesses = []
+        @incorrect_guesses = []
+        @dash_line = nil
+        @petal_art = ""
+        @your_guess = ""
+    end
 
-  def selection
-    index_choice = rand(@words.length  - 1)
-    @target_word_guess = @words[index_choice]
-    @target_array_form = @target_word_guess.split("")
-    print @target_word_guess
-  end
+    def selection
+        index_choice = rand(@words.length  - 1)
+        @target_word_guess = @words[index_choice]
+        @target_array_form = @target_word_guess.split("")
+        print @target_word_guess
+    end
 
-  def create_dash_line
-    number_dashes = @target_word_guess.length
-    dash_line = "-" * number_dashes
-    @dash_line = dash_line.chars.to_a
-    return @dash_line
-  end
+    def create_dash_line
+        number_dashes = @target_word_guess.length
+        dash_line = "-" * number_dashes
+        @dash_line = dash_line.chars.to_a
+        return @dash_line
+    end
 
-  def guess
+    def guess
 
-    print "\nType your guess: "
-    your_guess = gets.chomp.upcase
-    puts "\n"
-    puts "*" * 50
-    puts "\n"
-    if @target_word_guess.include?(your_guess)
-      @correct_guesses<<your_guess
-      @target_array_form.each_with_index do |char, index|
-        if your_guess == char
-          @dash_line[index].replace(char)
+        print "\nType your guess: "
+        @your_guess = gets.chomp.upcase
+        puts "\n"
+        puts "*" * 50
+        puts "\n"
+        if @target_word_guess.include?(your_guess)
+            @correct_guesses<<@your_guess
+            @target_array_form.each_with_index do |char,    index|
+                if @your_guess == char
+                    @dash_line[index].replace(char)
+                end
+            end
+            puts @petal_art
+        else
+            @incorrect_guesses<<@your_guess
+            @petal_art.slice!(0,3)
+            puts @petal_art
+            # puts @petal_art.sub!("(@)", "")
         end
-      end
-      puts @petal_art
-    else
-      @incorrect_guesses<<your_guess
-      @petal_art.slice!(0,3)
-      puts @petal_art
-      # puts @petal_art.sub!("(@)", "")
+        puts "Here is your target word: "
+        print @dash_line
+        puts "\n"
+        puts"Here are your incorrect guesses so far... \n"
+        print @incorrect_guesses
     end
-    puts "Here is your target word: "
-    print @dash_line
-    puts "\n"
-    puts"Here are your incorrect guesses so far... \n"
-    print @incorrect_guesses
-  end
 
-  def done?
-    @target_array_form.each do |let|
-      if !@correct_guesses.include?(let)
-        return false
-      end
+    def done?
+        @target_array_form.each do |let|
+            if !@correct_guesses.include?(let)
+                return false
+            end
+        end
+        return true
     end
-    return true
-  end
 
-  def graphic
-    @petal_art = "(@)(@)(@)(@)(@)\n,\\, \\, |,/, /,\n     _\\|/_\n    |_____|\n     |   |\n     |___|"
-    return @petal_art
-  end
-
-  def testing_guesses
-    puts @correct_guesses
-  end
-
-  def conclusion
-    until done? == true || @incorrect_guesses.length == 5
-      guess
+    def conclusion
+        until done? == true || @incorrect_guesses.length == 5
+            guess
+        end
+        if @incorrect_guesses.length == 5
+            puts"Oops you lose, nice try!"
+        elsif done? == true
+            puts "Good job, you win!"
+        end
     end
-    if @incorrect_guesses.length == 5
-      puts" Oops you lose, nice try!"
-    elsif done? == true
-      puts "Good job, you win!"
+
+    def correct_input?
+        alphabet = "A".."Z"
+        alphabet = alphabet.to_a
+        until @your_guess.length == 1 && alphabet.include?(@your_guess)
+            return false
+        end
+        return true
     end
-  end
+
+    def guessing
+        guess
+        until correct_input? == true
+            puts "Oops, you didn't enter one letter. Please try again...\n"
+            guess
+        end
+    end
+
+    def graphic
+        @petal_art = "(@)(@)(@)(@)(@)\n,\\, \\, |,/, /,\n     _\\|/_\n    |_____|\n     |   |\n     |___|"
+        return @petal_art
+    end
 end
 
 test = WordGuess.new
@@ -95,9 +109,6 @@ print test.create_dash_line
 puts "\n\n"
 puts test.graphic
 
-test.guess
+test.guessing
 
 test.conclusion
-
-
-# test.testing_guesses
