4bb1d04a82aba6587ae9f3b7694e1b03698197a9
Kate Shaffer <kate.shaffer@gmail.com>
2016-08-21 22:52:56 -0700

    ArtWork class defined.

--- art_work.rb
+++ art_work.rb
@@ -1,15 +1,29 @@
 class ArtWork
 
-  def initialize(countdown)
-    @countdown = countdown
+  def initialize
+
   end
 
-  def balloon_five
-    # puts "This is an image of five balloons. Really."
-    five_b = File.open("balloon_source.txt")
-    puts five_b.read
+  def print_ballons(countdown)
+    case countdown
+    when 5
+      five_b = File.open("five_balloons.txt")
+      puts five_b.read
+    when 4
+      four_b = File.open("four_balloons.txt")
+      puts four_b.read
+    when 3
+      three_b = File.open("three_balloons.txt")
+      puts three_b.read
+    when 2
+      two_b = File.open("two_balloons.txt")
+      puts two_b.read
+    when 1
+      one_b = File.open("one_balloon.txt")
+      puts one_b.read
+    when 0
+      zero_b = File.open("game_over.txt")
+      puts zero_b.read
+    end
   end
 end
-
-a = ArtWork.new(1)
-a.balloon_five
