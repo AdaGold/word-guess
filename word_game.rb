ef17a6ab8761032c3fe19ef1396c91cc0ccec679
Mary Morrison <marymorrison15@gmail.com>
2016-08-22 09:10:51 -0700

    word-game.rb update

--- word_game.rb
+++ word_game.rb
@@ -1,12 +1,13 @@
 class WordGame
-attr_accessor
+attr_accessor :cat
 
-def initialize(words)
+def initialize
 @cat = words[:cat]
-@dog = words[:dog]
+# @dog = words[:dog]
 
 end
 
 end
 
-words = {cat: ["C", "A", "T"], dog: ["D", "O", "G"]}
+words = {cat: ["C", "A", "T"]}
+# words = {cat: ["C", "A", "T"], dog: ["D", "O", "G"]}
