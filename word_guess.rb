9f13df5772fd93377cfe7f6d1f949c46d34b262b
Maria McGrew <mcg.maria@gmail.com>
2017-02-16 17:02:12 -0800

    have a working check guess method :)

--- word_guess.rb
+++ word_guess.rb
@@ -5,7 +5,7 @@ class Word_guess
 
   def initialize
     puts "new game initialized!"
-    @word = "paper"
+    @word = "PAPER"
     @board = Array.new(word.length) { |i| i = "_" }
     # @art = './turtle.txt'
     # @art = Art.new
@@ -17,11 +17,15 @@ class Word_guess
     puts "Welcome to Cythina & Maria's Word Guessing Game"
     puts "-" * 60
     puts "Guess the word before the turtle disapears"
+    puts "Here are your previous guesses: #{@letters_guess.join(", ") rescue ""}"
     # need to write the print_art method - will be in the class Art
     # print_art
-    # puts print_board
-    print_board
-    get_guess
+    puts print_board
+    #set a local variable guess to get_guess (which returns the letter entered by user)
+    guess = get_guess
+    #have to send the local variable guess to method check_guess so this method has guess
+    check_guess(guess)
+    store_guess(guess)
   end
 
   def print_board
@@ -30,8 +34,28 @@ class Word_guess
 
   def get_guess
     puts "What letter would you like to guess? "
-    guess = gets.chomp.upcase
-    return guess
+    return gets.chomp.upcase
+  end
+
+  def store_guess(guess)
+    @letters_guessed << guess
+  end
+
+  def check_guess(guess)
+    # needs to check if the letter guessed matches a letter in @word
+    #board = [_ _ _ _ _] vs. word="PAPER" would word = [P, A, P, E, R]
+    #array method .split(TELL ME WHERE TO SPLIT) - turns a string into an array
+    #use replace if index of each matches
+    final_board = @word.split("")
+    # if guess is included in final board / word
+    #we need the guess to show up on board in the correct spot
+    final_board.each_index do |i|
+      if final_board[i] == guess
+        @board[i] = guess
+      end
+    end
+    print @board
+    puts
   end
 
 end
