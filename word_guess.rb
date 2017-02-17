664cdaa675cc1b664c60a317f6594d840eba0c31
Maria McGrew <mcg.maria@gmail.com>
2017-02-16 16:25:58 -0800

    Added methods main, print_board, and get_guess, working

--- word_guess.rb
+++ word_guess.rb
@@ -13,9 +13,32 @@ class Word_guess
     @max_guesses = 12
   end
 
+  def main
+    puts "Welcome to Cythina & Maria's Word Guessing Game"
+    puts "-" * 60
+    puts "Guess the word before the turtle disapears"
+    # need to write the print_art method - will be in the class Art
+    # print_art
+    # puts print_board
+    print_board
+    get_guess
+  end
+
+  def print_board
+    return "Word: #{@board.join(" ")}"
+  end
+
+  def get_guess
+    puts "What letter would you like to guess? "
+    guess = gets.chomp.upcase
+    return guess
+  end
+
 end
 
 class Art
 end
 
 new_game = Word_guess.new
+print new_game.main
+puts ""
