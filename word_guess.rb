bb3755148b0474a0fb8f3f1281da82ea41e7ddd0
Tamira Vojnar <tamiravojnar@gmail.com>
2017-08-17 17:40:28 -0700

    Functioning for primary requirements, end of class

--- word_guess.rb
+++ word_guess.rb
@@ -25,15 +25,15 @@
 #ASCII-art in its own class? Or just within the word class
 
 
-# require 'faker'
-#
-# words = []
-# 10.times do
-#   words << Faker::HarryPotter.location
-# end
-#
-# puts words
+###########################
+#Things to do
+  #for difficulty level have three different arrays of different length words, case statement to pick from one of them depending on what difficulty user wants
+  #allow the user to enter the whole word
 
+
+
+require_relative 'word_guess_acii'
+require 'colorize'
 class Word
   attr_reader :word, :word_array, :dash_array, :letters_guesses
   def initialize(word)
@@ -58,53 +58,89 @@ class Word
   end#check_if_in_word
 
   def user_display
-    win?
-    lose?
+    win
+    lose
+    puts  "Word you need to guess: #{@dash_array.join(' ')}".colorize(:blue), "Letters you have guessed: #{@letters_guesses.join(' ')}".colorize(:purple), "You have #{@remaining_guesses} remaining guesses.".colorize(:green)
+    case @remaining_guesses
+    when 5
+      puts five
+    when 4
+      puts four
+    when 3
+      puts three
+    when 2
+      puts two
+    when 1
+      puts one
+    end
 
-    return @dash_array.join(' '), "You have #{@remaining_guesses} remaining guesses.", "Letters you have guessed: #{@letters_guesses.join(' ')}"
     # ASCII-art
     # case statement for number of guesses left to change art
   end
 
   private
 
-  def win?
+  def win
     if @dash_array == @word_array
-      return "You win!" #Add ASCII-art rainbows
-      exit
+      puts "\nYou won!"
+      puts you_win
+     exit #Add ASCII-art rainbows,
     end
   end
 
-  def lose?
+  def lose
     if @remaining_guesses == 0
-      return "You lose" #Add ASCII-art sad
-      exit
+      puts "\nYou lost"
+      puts you_lose
+       exit  #Add ASCII-art sad
     end
   end
 
 
 end
 
+
 #Generating a word for the user to guess
 #Need to add in more words later
-random_words =["WATCH"]
+random_words =["WATCH", "TABLE", "CHAIR", "COMPUTER"]
 sample_word = Word.new(random_words.sample)
 
 #Getting a letter from the user
 #Still need to do varification that is is a letter and that it has not already been guessed
-
+loop do
+puts "Welcome to the word-guess game!"
 puts "Please enter a letter: "
   user_input = gets.chomp.upcase
 
+#Defines what special characters are
+  special = "?<>',?[]}{=-)(*&^%$#`~{}"
+  regex = /[#{special.gsub(/./){|char| "\\#{char}"}}]/
+
 #verify user input
 until ((user_input.length == 1) && (/[\d]/.match(user_input) == nil)) && !(sample_word.letters_guesses.include?(user_input))
-  puts "Please enter a letter: "
+  if !(/[\d]/.match(user_input) == nil) && user_input.to_i.to_s == user_input
+    puts "No numbers!"
+  elsif user_input.length > 1 && !(/[\d]/.match(user_input) == nil)
+    puts "Please enter a single letter with no numnbers!"
+  elsif user_input.length > 1 && /[\d]/.match(user_input) == nil
+    puts "Please enter a single letter at a time!"
+  elsif sample_word.letters_guesses.include?(user_input)
+    puts "You already guessed that letter! Please enter a different letter!"
+  elsif !(/[\d]/.match(user_input) == nil)
+    puts "Please don't enter a number! Enter a letter!"
+  end
   user_input = gets.chomp.upcase
 end
 
-test = sample_word.check_if_in_word(user_input)
-# puts test
+#Check if letter is in the word, and change @dash_array or @remaining_guesses
+sample_word.check_if_in_word(user_input)
+
 puts sample_word.user_display
+# puts sample_word.dash_array == sample_word.word_array
+# puts sample_word.dash_array.join(' ')
+# puts sample_word.word_array.join(' ')
+end#loop
+
 
 
 # puts sample_word.word
